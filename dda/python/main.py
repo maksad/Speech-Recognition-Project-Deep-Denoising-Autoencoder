6f8435fa0f52a8491543c4fa32c11781da26d58a
maksad <d.maksad@gmail.com>
2019-12-09 16:59:16 +0200

    Refactor main

--- dda/python/main.py
+++ dda/python/main.py
@@ -8,49 +8,32 @@ from os.path import join
 from utils import search_wav, get_best_data
 from sklearn.model_selection import train_test_split
 
+import argparse
+
+parser = argparse.ArgumentParser()
+paser.add_argument('--training_noisy_dir', type=str, default='./data/train_data/noisy')
+paser.add_argument('--training_clean_dir', type=str, default='./data/train_data/clean')
+paser.add_argument('--enhanced_dir', type=str, default= './data/enhanced')
+paser.add_argument('--model_dir', type=str, default='./model')
+paser.add_argument('--feat', type=str, default='spec', help='spec/ mel/ mfcc')
+paser.add_argument('--train_task', type=str, default='same_noise', help='set task name for noting your dataset')
+paser.add_argument('--train', type=str, default=True)
+paser.add_argument('--ncores', type=str, default=20)
+paser.add_argument('--epochs', type=str, default=20)
+paser.add_argument('--batch_size', type=str, default=32)
+paser.add_argument('--lr', type=str, default=1e-3)
+args = parser.parse_args()
+
+
 np.random.seed(1234567)
 
-best_noise_data = [
-    '1-4211-A-12.wav',
-    '1-7456-A-13.wav',
-    '1-9841-A-13.wav',
-    '1-17367-A-10.wav',
-    '1-18527-A-44.wav',
-    '1-18527-B-44.wav',
-    '1-19840-A-36.wav',
-    '1-19872-B-36.wav',
-    '1-21189-A-10.wav',
-    '1-21896-A-35.wav',
-    '1-23996-B-35.wav',
-    '1-24074-A-43.wav',
-    '1-27166-A-35.wav',
-    '1-29532-A-16.wav',
-    '1-32373-B-35.wav',
-    '1-47709-A-16.wav',
-    '3-181278-A-22.wav',
-    '5-221567-A-22.wav',
-    '5-262957-A-22.wav',
-    '1-11687-A-47.wav',
-]
-best_training_data = [
-    '/fin_words/S03/',
-    '/fin_words/S06/',
-    '/fin_words/S11/',
-    '/fin_words/S15/',
-    '/fin_words/S20/',
-    '/fin_words/S22/',
-    '/fin_words/S26/',
-    '/fin_words/S27/',
-    '/fin_words/S38/',
-]
 
-
-def synthesize_noisy_data(clean_dir, noise_dir, noisy_dir, feat, ncores):
+def synthesize_noisy_data():
     # ===========================================================
     # ===========       Synthesize Noisy Data        ============
     # ===========================================================
-    clean_file_list = get_best_data(search_wav(clean_dir), best_training_data)
-    noise_file_list = get_best_data(search_wav(noise_dir), best_noise_data)
+    clean_file_list = search_wav(clean_dir)
+    noise_file_list = search_wav(noise_dir)
 
     clean_train_list, clean_test_list = train_test_split(clean_file_list, test_size=0.2)
     noise_train_list, noise_test_list = train_test_split(noise_file_list, test_size=0.2)
@@ -101,24 +84,9 @@ def create_training_matrix(noisy_dir, feat):
 
 
 def main():
-    clean_dir = '../data_fin_words/fin_words'
-    noise_dir = '../data/raw/noise' # use the normal noise
-    noisy_dir =  '../fin_data/noisy'
-    enhanced_dir =  '../fin_data/enhanced'
-    tb_dir = '../fin_model/tb_logs'
-    saver_dir = '../fin_model/saver'
-    TRAIN = True
-    TEST = True
-    ncores = 20
-    epochs = 20
-    batch_size = 32
-    lr = 1e-3
-    feat = 'spec'; # spec/ mel/ mfcc
-    train_task = 'same_noise'  # set task name for noting your dataset
+    synthesize_noisy_data()
 
-    synthesize_noisy_data(clean_dir, noise_dir, noisy_dir, feat, ncores)
-
-    create_training_matrix(noisy_dir, feat)
+    create_training_matrix()
 
     # ===========================================================
     # ===========             Main Model             ============
