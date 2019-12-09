87067f2909bfb887276a9e89d68a9848ac675b02
maksad <d.maksad@gmail.com>
2019-12-09 15:30:21 +0200

    Creat 'create_training_matrix', 'synthesize_noisy_data' functions

--- dda/python/main.py
+++ dda/python/main.py
@@ -44,6 +44,62 @@ best_training_data = [
     '/fin_words/S38/',
 ]
 
+
+def synthesize_noisy_data(clean_dir, noise_dir, noisy_dir, feat, ncores):
+    # ===========================================================
+    # ===========       Synthesize Noisy Data        ============
+    # ===========================================================
+    clean_file_list = get_best_data(search_wav(clean_dir), best_training_data)
+    noise_file_list = get_best_data(search_wav(noise_dir), best_noise_data)
+
+    clean_train_list, clean_test_list = train_test_split(clean_file_list, test_size=0.2)
+    noise_train_list, noise_test_list = train_test_split(noise_file_list, test_size=0.2)
+    noise_test_list = noise_train_list # test on the same noise
+
+    print('--- Synthesize Training Noisy Data ---')
+    train_noisy_dir = join(noisy_dir, 'train')
+    sr_clean = 16000
+    sr_noise = 44100
+    snr_list = ['20dB', '10dB', '0dB']
+    data_num = None  # set data_num to make training data numbers for different snr
+    syn_train = Synth(clean_train_list, noise_train_list, feat, sr_clean, sr_noise)
+    syn_train.gen_noisy(
+        snr_list, train_noisy_dir, data_num=data_num, ADD_CLEAN=True, cpu_cores=ncores
+    )
+    print('--- Synthesize Testing Noisy Data ---')
+    test_noisy_dir = join(noisy_dir, 'test')
+    sr_clean = 16000
+    sr_noise = 44100
+    data_num = None # set data_num to make testing data numbers for different snr
+    snr_list = ['15dB']
+    syn_test = Synth(clean_test_list, noise_test_list, feat,  sr_clean, sr_noise)
+    syn_test.gen_noisy(
+        snr_list, test_noisy_dir, data_num=data_num, ADD_CLEAN=True, cpu_cores=ncores
+    )
+
+
+def create_training_matrix(noisy_dir, feat):
+    # ===========================================================
+    # ===========       Create Training Matrix       ============
+    # ===========================================================
+    print('--- Generate Training Matrix ---')
+    train_task = 'same_noise'  # set task name for noting your dataset
+    training_files_dir = '../fin_data/training_files';
+    train_noisy_dir = join(noisy_dir, 'train');
+    DEL_TRAIN_WAV = True;
+    gen_mat = GenMatrix(training_files_dir, train_task, train_noisy_dir);
+    split_num = 50  # number of spliting files
+    iter_num = 2  # set iter number to use multi-processing, cpu_cores = split_num/iter_num
+    input_sequence = False  # set input data is sequence or not
+    gen_mat.create_h5(
+        split_num=split_num,
+        iter_num=iter_num,
+        feat=feat,
+        input_sequence=input_sequence,
+        DEL_TRAIN_WAV=DEL_TRAIN_WAV
+    )
+
+
 def main():
     clean_dir = '../data_fin_words/fin_words'
     noise_dir = '../data/raw/noise' # use the normal noise
@@ -60,52 +116,9 @@ def main():
     feat = 'spec'; # spec/ mel/ mfcc
     train_task = 'same_noise'  # set task name for noting your dataset
 
-    # ===========================================================
-    # ===========       Synthesize Noisy Data        ============
-    # ===========================================================
-    clean_file_list = get_best_data(search_wav(clean_dir), best_training_data)
-    noise_file_list = get_best_data(search_wav(noise_dir), best_noise_data)
+    synthesize_noisy_data(clean_dir, noise_dir, noisy_dir, feat, ncores)
 
-    clean_train_list, clean_test_list = train_test_split(
-        clean_file_list, test_size=0.2)
-
-    noise_train_list, noise_test_list = train_test_split(
-        noise_file_list, test_size=0.2)
-    noise_test_list = noise_train_list # test on the same noise
-
-    print('--- Synthesize Training Noisy Data ---')
-    train_noisy_dir = join(noisy_dir, 'train')
-    sr_clean = 16000
-    sr_noise = 44100
-    snr_list = ['20dB', '10dB', '0dB']
-    data_num = None  # set data_num to make training data numbers for different snr
-    syn_train = Synth(clean_train_list, noise_train_list, feat, sr_clean, sr_noise)
-    syn_train.gen_noisy(snr_list, train_noisy_dir,
-                        data_num=data_num, ADD_CLEAN=True, cpu_cores=ncores)
-    print('--- Synthesize Testing Noisy Data ---')
-    test_noisy_dir = join(noisy_dir, 'test')
-    sr_clean = 16000
-    sr_noise = 44100
-    data_num = None # set data_num to make testing data numbers for different snr
-    snr_list = ['15dB']
-    syn_test = Synth(clean_test_list, noise_test_list, feat,  sr_clean, sr_noise)
-    syn_test.gen_noisy(snr_list, test_noisy_dir,
-                        data_num=data_num, ADD_CLEAN=True, cpu_cores=ncores)
-    # ===========================================================
-    # ===========       Create Training Matrix       ============
-    # ===========================================================
-    print('--- Generate Training Matrix ---')
-    train_task = 'same_noise'  # set task name for noting your dataset
-    training_files_dir = '../fin_data/training_files';
-    train_noisy_dir = join(noisy_dir, 'train');
-    DEL_TRAIN_WAV = True;
-    gen_mat = GenMatrix(training_files_dir, train_task, train_noisy_dir);
-    split_num = 50  # number of spliting files
-    iter_num = 2  # set iter number to use multi-processing, cpu_cores = split_num/iter_num
-    input_sequence = False  # set input data is sequence or not
-    gen_mat.create_h5(split_num=split_num, iter_num=iter_num, feat=feat,
-                      input_sequence=input_sequence,
-                      DEL_TRAIN_WAV=DEL_TRAIN_WAV)
+    create_training_matrix(noisy_dir, feat)
 
     # ===========================================================
     # ===========             Main Model             ============
@@ -126,10 +139,8 @@ def main():
     result_dir = '../fin_data/enhanced/{}_{}/'.format(note, date)
     num_test = 30 # Set this number to decide how many testing data you wanna use. (None => All)
     cpu_cores = 30
-    test_saver = '{}_{}/{}/best_saver_{}'.format(
-        saver_dir, note, date, train_task)
-    model.test(testing_data_dir, result_dir, feat,
-               test_saver, cpu_cores, num_test)
+    test_saver = '{}_{}/{}/best_saver_{}'.format(saver_dir, note, date, train_task)
+    model.test(testing_data_dir, result_dir, feat, test_saver, cpu_cores, num_test)
 
 
 if __name__ == '__main__':
